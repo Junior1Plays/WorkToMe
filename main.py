665702f2a74951d5af3759d07e12159ad012ca19
Junior Plays <50379079+Junior1Plays@users.noreply.github.com>
2022-07-31 20:49:08 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+import requests
+from time import sleep
+channelid = input('Channel ID: ')
+url = 'https://discord.com/api/v9/channels/' + channelid + '/messages'
+token = input('Token: ')
+
+def work(url, tkn):
+    sleep(300)
+    header = {
+         'authorization': tkn
+    }
+    payload = {
+       'content': '_work'
+    }
+    rqst = requests.post(url, headers=header, data=payload)
+    work(url, tkn)
+work(url, token)n)
+work(url, token) tkn)
+work(url, token)ders=header, data=payload)
+    work(url, tkn)
+work(url, token)
